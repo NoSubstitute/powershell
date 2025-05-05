e464bc96d6abbb9d98c554ea81e2f7ee65fdc37f
Kim Nilsson <github@no-substitute.com>
2025-05-05 16:02:28 +0200

    Create SetInteropAddresses.ps1
    Just some placeholder text describing the problem

---
+++ SetInteropAddresses.ps1
@@ -0,0 +1,7 @@
+## I will put the script's content here tomorrow.
+## This script doesn't work anymore, as it used the now deprecated Azure AD APIs, replaced by MS Graph APIs.
+## I need help converting the AAD parts.
+
+## The Exchange API parts are working fine.
+## So it deleted with ease about 1500 Contacts from Exchange, as the AAD part wasn't able to List Users to the array.
+## Since the array was empty, the script was supposed to delete those contacts, which it did. :-)
